f002cb2cfda4b67062bad5e518e5fa8eb4d91a06
Joshua Paul <joshpauljtp@gmail.com>
2023-08-25 14:28:12 +0530

    Everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Iframe from "./iframe/page";
-import axios from "axios";
 import UserInput from "./components/UserInput";
 import { useEffect, useState } from "react";
 import Computation from "./components/Computation";
@@ -10,18 +9,9 @@ import { reactQueryClient } from "./config/reactQueryClient";
 import "./globalStyles.css";
 
 const Home = () => {
-  const [devData, setDevData] = useState(null);
   const [activeStep, setActiveStep] = useState(0);
   const [devLink, setDevLink] = useState(null);
   const [designLink, setDesignLink] = useState(null);
-
-  const { data, mutate, isLoading } = useMutation(() => fetchApiData());
-
-  const fetchApiData = () => {
-    return axios.post("/api/ai", {
-      devData,
-    });
-  };
 
   useEffect(() => {
     if (activeStep === 0 && devLink !== null && designLink !== null) {
@@ -29,17 +19,10 @@ const Home = () => {
     }
   }, [activeStep, devLink, designLink]);
 
-  if (isLoading) return <p>Loading...</p>;
-
   return (
     <QueryClientProvider client={reactQueryClient}>
-      <p>{JSON.stringify(data?.data?.res?.text)}</p>
       {activeStep === 0 && (
-        <UserInput
-          setDevLink={setDevLink}
-          setDesignLink={setDesignLink}
-          mutate={mutate}
-        />
+        <UserInput setDevLink={setDevLink} setDesignLink={setDesignLink} />
       )}
       {activeStep === 1 && (
         <Computation
@@ -49,7 +32,6 @@ const Home = () => {
         />
       )}
       {activeStep === 2 && <Results />}
-      <Iframe onRefLoad={(ref: any) => setDevData(ref)} />
     </QueryClientProvider>
   );
 };
