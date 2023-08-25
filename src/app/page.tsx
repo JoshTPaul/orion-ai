bbc7728d43efc6b94921935899fa80cd40a10bed
kiran-nambiar <kiran.nambiar2@tifin.com>
2023-08-25 14:12:11 +0530

    reformatted the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
 "use client";
-import { styled } from "styled-components";
 import Iframe from "./iframe/page";
 import axios from "axios";
 import UserInput from "./components/UserInput";
 import { useEffect, useState } from "react";
 import Computation from "./components/Computation";
 import Results from "./components/Results";
-import { QueryClientProvider } from "react-query";
-import HomeLayer from "./HomeLayer";
+import { QueryClientProvider, useMutation } from "react-query";
 import { reactQueryClient } from "./config/reactQueryClient";
 
 const Home = () => {
-  const onRefLoad = (ref: any) => {
-    console.log("ref", ref.background);
-    axios.post("/api/ai", {
-      ref,
-    });
-  };
-
+  const [devData, setDevData] = useState(null);
   const [activeStep, setActiveStep] = useState(0);
   const [devLink, setDevLink] = useState(null);
   const [designLink, setDesignLink] = useState(null);
+
+  const { data, mutate, isLoading } = useMutation(() => fetchApiData());
+
+  const fetchApiData = () => {
+    return axios.post("/api/ai", {
+      devData,
+    });
+  };
 
   useEffect(() => {
     if (activeStep === 0 && devLink !== null && designLink !== null) {
@@ -28,10 +28,17 @@ const Home = () => {
     }
   }, [activeStep, devLink, designLink]);
 
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <QueryClientProvider client={reactQueryClient}>
+      <p>{JSON.stringify(data?.data?.res?.text)}</p>
       {activeStep === 0 && (
-        <UserInput setDevLink={setDevLink} setDesignLink={setDesignLink} />
+        <UserInput
+          setDevLink={setDevLink}
+          setDesignLink={setDesignLink}
+          mutate={mutate}
+        />
       )}
       {activeStep === 1 && (
         <Computation
@@ -41,7 +48,7 @@ const Home = () => {
         />
       )}
       {activeStep === 2 && <Results />}
-      {/* <HomeLayer /> */}
+      <Iframe onRefLoad={(ref: any) => setDevData(ref)} />
     </QueryClientProvider>
   );
 };
