8f0b63d89743728830e30a4aa1873e1d788549f6
Joshua Paul <joshpauljtp@gmail.com>
2023-12-01 11:53:35 +0530

    Removing Bard fallback

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request, res: any) {
   const msg = await msgConstructor(inputArr);
   try {
     const api = new ChatGPTAPI({
-      apiKey: "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",
+      apiKey: "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",
       completionParams: {
         temperature: 0,
       },
@@ -22,22 +22,9 @@ export async function POST(req: Request, res: any) {
       { status: res?.data?.status }
     );
   } catch (error: any) {
-    try {
-      const myBard = new Bard({
-        "__Secure-1PSID":
-          "aAiChKed0OGNV4Tz6_pOhA0VcU_MhH8JpSvv0sxsVtZYhOOvUmCAkHoxQZ8apCKQTpqyEw.",
-        "__Secure-1PSIDTS":
-          "sidts-CjIBSAxbGTxL1ZRig6F7o9UrHpBU2GyYEvMZlpywvBxdFCCfqvawSZGVXeOmE9jKhheW8xAA",
-        "__Secure-1PSIDCC":
-          "APoG2W_fh55GishXZ-aApMRDAptx7PhVt3N8FaXV6FAsCTWeWEHXxZCtV1RL4VofPAGspQ8z0vA",
-      });
-      const response = await myBard.ask(msg);
-      return NextResponse.json({ error: response }, { status: 200 });
-    } catch (error: any) {
-      return NextResponse.json(
-        { error: `Something went wrong ${error}` },
-        { status: error?.response?.status }
-      );
-    }
+    return NextResponse.json(
+      { error: `Something went wrong ${error}` },
+      { status: error?.response?.status }
+    );
   }
 }
